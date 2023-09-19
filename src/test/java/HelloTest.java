6a60a28f82f9923c6da4818ce6f56ba90f930c4c
gramesh13g <143886265+gramesh13g@users.noreply.github.com>
2023-09-20 02:31:50 +0530

    Update HelloTest.java

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -7,6 +7,7 @@ public class HelloTest {
 
     @Test
     public void hello_first_test_case() {
+        public void hello_first_test_case() {
         assertTrue(true);
 //        fail("เขียนชุดการทดสอบสิ");
     }
